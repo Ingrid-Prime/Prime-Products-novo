e00a21e9a29fe8f76d8c9e2a20f5c0f7dcbcbb43
Ingrid-Prime <info@primeproducts.ind.br>
2026-07-18 00:12:08 -0300

    fix: atualizar imagem principal de detectores para detector-principal.jpg

--- src/data/catalogs/index.ts
+++ src/data/catalogs/index.ts
@@ -1,7 +1,8 @@
-﻿export { TEKNO_VALVES_CATALOG } from './products/teknoValvesCatalog';
+export { TEKNO_VALVES_CATALOG } from './products/teknoValvesCatalog';
 export { TKF_CATALOG } from './products/tkfCatalog';
 export { VALVES_CATALOG } from './products/valvesCatalog';
 export { TRANSMITTER_CATALOG } from './products/transmitterCatalog';
 export { CALIBRATION_CATALOG } from './products/calibrationCatalog';
 export { SPECIAL_REGULATORS_CATALOG } from './products/specialRegulatorsCatalog';
 export { HYDRAULIC_CATALOG } from './products/hydraulicCatalog';
+export { GASTRON_CATALOG } from './products/gastronCatalog';
---
+++ src/data/catalogs/products/gastronCatalog.ts
@@ -0,0 +1,22 @@
+export const GASTRON_CATALOG = [
+  {
+    title: 'Modelos Principais - Detectores de Vazamento',
+    columns: ['Modelo', 'Tipo de Gás', 'Display', 'Sinal de Saída', 'Certificação', 'Imagem'],
+    rows: [
+      ['GTD-2000', 'Gases Inflamáveis / Tóxicos', 'OLED', '4-20mA, HART', 'ATEX, IECEx, SIL2', '<img src="/images/produtos/detectores-de-vazamentos/image10.png" class="h-16 object-contain" />'],
+      ['GTD-5000', 'Gases Tóxicos (Baixa Concentração)', 'LCD Colorido', '4-20mA, RS-485', 'ATEX, IECEx', '<img src="/images/produtos/detectores-de-vazamentos/image66.png" class="h-16 object-contain" />'],
+      ['GIR-3000', 'Gases Combustíveis (Infravermelho)', 'LED / LCD', '4-20mA, HART, RS-485', 'ATEX, SIL2', '<img src="/images/produtos/detectores-de-vazamentos/image65.png" class="h-16 object-contain" />'],
+      ['GTD-3000', 'Monitoramento Multi-gás', 'OLED Local', '4-20mA, HART', 'KCs, ATEX', '<img src="/images/produtos/detectores-de-vazamentos/image9.png" class="h-16 object-contain" />'],
+      ['GTD-1000', 'Detector Portátil', 'LCD', 'Alarmes Locais', 'CE, FCC', '<img src="/images/produtos/detectores-de-vazamentos/image111.png" class="h-16 object-contain" />'],
+    ]
+  },
+  {
+    title: 'Painéis e Sistemas de Controle (HMI & PLC)',
+    columns: ['Modelo', 'Canais', 'Comunicação', 'Alimentação', 'Montagem', 'Imagem'],
+    rows: [
+      ['GTC-200A', 'Até 16 Canais', 'Modbus RS-485', '110/220V AC', 'Painel Rack', '<img src="/images/produtos/detectores-de-vazamentos/image158.png" class="h-16 object-contain" />'],
+      ['GTC-500', 'Multicanal (Até 64)', 'Ethernet, RS-485', '24V DC', 'Parede / Painel', '<img src="/images/produtos/detectores-de-vazamentos/image361.png" class="h-16 object-contain" />'],
+      ['Sistema HMI', 'Customizado', 'TCP/IP', 'Bivolt', 'Gabinete Dedicado', '<img src="/images/produtos/detectores-de-vazamentos/image223.png" class="h-16 object-contain" />']
+    ]
+  }
+];
